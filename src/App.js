5e63ec90ef6a160bc881850c29e7a86a0bfa50b3
Robber1922 <arshinovade@gmail.com>
2022-05-02 01:26:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.module.scss';
+import Layout from './hoc/Layout/Layout'
+import Quiz from "./containers/Quiz/Quiz";
+
+function App() {
+  return (
+    <Layout>
+      <Quiz></Quiz>
+    </Layout>
+  );
+}
+
+export default App;
